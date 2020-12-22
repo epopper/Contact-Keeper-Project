855e332ae73f9dd58afaf92b71db6cf58e8a95ca
Eric Popper <eric.popper@gmail.com>
2020-12-22 15:36:43 -0800

    First commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the ContactKeeper API..." })
+);
+
+// Defining Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
